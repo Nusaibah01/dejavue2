07433e603bf361407ba330c29ce315365f6311a9
Nusaibah01 <charifnusaibah01@gmail.com>
2024-05-23 11:22:20 +0200

    stringexamples

---
+++ javascript_workshop/stringexamples/script/code.js
@@ -0,0 +1,28 @@
+// String Examples:
+let sentence = 'I love programming'
+// String size 
+console.log(`Number of characters: ${sentence.length}`);
+// First Character
+console.log(`First character : ${sentence[0]}`);
+// Last Character 
+console.log(`Last character: ${sentence[numOfChars - 1]}`);
+// Check if the first character starts with I vs i
+console.log(`Start with I? ${sentence.startsWith(I)}`);
+// Replace love with hate
+console.log(`Replace love with hate ${sentence.replace('love', 'hate')}`);
+console.log(`Does it end with programming?  ${sentence.endsWith('programming')}`);
+console.log(`includes: ${sentence.includes('love')}`);
+/*Please work on the following string methods:
+-split()
+-slice()
+-charAt()
+-concat()
+-indexOf()
+- lastIndexOf()
+- substring()
+- toUpperCase()
+- trim()
+- trimStart()
+- trimEnd()
+*/
+// split()
