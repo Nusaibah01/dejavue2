926e3f4b9e2f08803e77e524658f88ace5bb4375
Nusaibah01 <charifnusaibah01@gmail.com>
2024-06-03 10:13:26 +0200

    update

---
+++ javascript_workshop/m-s/script/code.js
@@ -0,0 +1,9 @@
+// Functionality
+function convertMinutes2Seconds(){
+let minutes = parseInt(document.getElementById(input).value)
+let seconds = minutes * 60
+
+// Display
+document.getElementById(answer).innerHTML = {seconds}
+
+}
