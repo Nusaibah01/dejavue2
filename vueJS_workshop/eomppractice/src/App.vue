04d0bcbe0f5590cffea26cab17d62e47415e15ca
Nusaibah01 <charifnusaibah01@gmail.com>
2024-06-26 11:43:51 +0200

    footerSetup

--- vueJS_workshop/eomppractice/src/App.vue
+++ vueJS_workshop/eomppractice/src/App.vue
@@ -4,13 +4,16 @@
 <main class="container-fluid">
   <router-view/>
 </main>
+<Footer />
 </div>
 </template>
 <script>
 import NavBar from '@/components/NavBar.vue'
+import Footer from '@/components/Footer.vue'
 export default {
   components:{
-    NavBar
+    NavBar,
+    Footer
   }
 }
 </script>
